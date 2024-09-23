962db563e54adcdf15afde88f036c749203ec35c
brunoprestesap <brunoasprestes@gmail.com>
2024-09-23 14:23:57 -0300

    App inicial

---
+++ interpretaRegistro.js
@@ -0,0 +1,41 @@
+import crc from "crc"
+import moment from "moment"
+import fs from 'fs'
+
+moment.locale('pt-br');
+
+function interpretarRegistro(linha) {
+  linha = linha.replace(/\r\n/g, "\n");
+  
+  try {
+    const tipoRegistro = linha.substring(9, 10);
+    if (tipoRegistro !== "3") {
+      return null;
+    }
+
+    const NSR = linha.substring(0, 9).trim();
+    const dataHoraStr = linha.substring(10, 34);
+    const CPF = linha.substring(34, 46).trim();
+    const crc16 = crc
+      .crc16xmodem(linha.substring(0, 46))
+      .toString(16)
+      .toUpperCase();
+
+    const dados = {
+      NSR,
+      TipoRegistro: "3 - MARCAÇÃO DE PONTO",
+      DataHora: moment(dataHoraStr).toDate(), // Formata data e hora
+      CPF,
+      CRC16: crc16.padStart(4, "0"), // Preenche com zeros à esquerda se necessário
+    };
+
+    fs.writeFileSync('nsr.json', JSON.stringify(dados));
+
+    return dados;
+  } catch (error) {
+    console.error("Erro ao interpretar registro:", error, linha);
+    return null;
+  }
+}
+
+export { interpretarRegistro }
