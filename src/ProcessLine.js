8658f0447a84abc1eda5b06d2c4bcf564d29100c
gabrielfelipedy <gabriel.felipe.dy@gmail.com>
2025-04-17 17:45:14 -0300

    write first tests

--- src/ProcessLine.js
+++ src/ProcessLine.js
@@ -1,25 +1,25 @@
-import crc from "crc"
 import moment from 'moment-timezone'
-import fs from 'fs'
 
 moment().tz("America/Sao_Paulo");
 
 //INTERPRETA SOMENTE OS REGISTROS DE PONTO
 export async function processLine(linha) {
 
-  //TRATA AS QUEBRAS DE LINHA
+  //TRATA AS QUEBRAS DE LINHA E ESPAÇOS
   linha = linha.replace(/\r\n/g, "\n").trim();
   
   try 
   {
     const tipoRegistro = linha.substring(9, 10);
 
-    if (tipoRegistro !== "3") {
+    if (!tipoRegistro || tipoRegistro !== "3") {
       return null;
     }
 
     const regist = linha.substring(0, 46).trim()
     const crc = linha.substring(46).trim()
+
+    if(!regist || !crc) return null
 
     //remove extra zero in middle of string
     const line  = `${regist.slice(0, 34) + regist.slice(35)} ${crc}`
